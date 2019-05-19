94dddda9abddcf5f55c236cb51de139850a5c826
Case Delst <casedelst@gmail.com>
2019-05-19 02:46:10 -0700

    Add average average line

--- ping.py
+++ ping.py
@@ -1,3 +1,4 @@
+from    matplotlib.widgets    import     Slider, Button, RadioButtons
 import  matplotlib.pyplot     as         plt 
 import  matplotlib.animation  as         animation 
 import  matplotlib.patches    as         mpatches
@@ -8,6 +9,7 @@ import  pandas                as         pd
 import  numpy                 as         np
 import  os
 
+
 plt.rcParams.update({'font.size': 12})
 style.use('dark_background')
 fig = plt.figure()
@@ -15,19 +17,21 @@ ax1 = fig.add_subplot(1,1,1)
 df = pd.DataFrame(columns=['Time', 'Min', 'Avg', 'Max', 'Label?'])
 
 #Number of pings available on graph at any given time
-targetXTicks = 51
+targetXTicks = 60
 labelSeparator = 15
+lineWeight = 2
+yMax = 80
+yMin = 0
 
 #Initialize DF
 for i in range(targetXTicks):
-    LabelVal = True if ((51 - i) % labelSeparator == 0) else False
-    df = df.append({'Time'   : "-00:00:" + (str(51 - i) if (51 - i) >= 10 else ('0' + str(51 - i))), 
-                    'Min'    : 1, 
-                    'Avg'    : 1, 
-                    'Max'    : 1, 
+    LabelVal = True if ((targetXTicks - i) % labelSeparator == 0) else False
+    df = df.append({'Time'   : "-00:00:" + (str(targetXTicks - i) if (targetXTicks - i) >= labelSeparator else ('0' + str(targetXTicks - i))), 
+                    'Min'    : 0, 
+                    'Avg'    : 0, 
+                    'Max'    : 0, 
                     'Label?' : LabelVal},
                     ignore_index=True)
-    
 
 def animate(i):
     global df
@@ -40,8 +44,6 @@ def animate(i):
         pingStats[i] = float(pingStats[i])
 
     #Add to already existing data frame
-    
-    
     currentTime = str(datetime.now()).split(' ')[1][0 : -7]
     secondsVal = int(currentTime[-2:])
     LabelVal = True if (secondsVal % labelSeparator == 0) else False
@@ -61,15 +63,29 @@ def animate(i):
     df = dfAppend
     print()
 
+    
+
     xs = []
     ys = []
 
     ax1.clear()
-    ax1.plot(df['Time'], df['Min'], label='Min', linewidth=2, color='green')
-    ax1.plot(df['Time'], df['Avg'], label='Avg', linewidth=2, color='orange')
-    ax1.plot(df['Time'], df['Max'], label='Max', linewidth=2, color='red')
+    ax1.plot(df['Time'], df['Min'], label='Min', linewidth=lineWeight, color='green')
+    ax1.plot(df['Time'], df['Avg'], label='Avg', linewidth=lineWeight, color='orange')
+    ax1.plot(df['Time'], df['Max'], label='Max', linewidth=lineWeight, color='red')
+    #Find average average ping
+    averageSum = df.sum(axis=0, skipna=True)['Avg']
+    averageAverage = averageSum / targetXTicks
+    ax1.axhline(y=averageAverage, 
+                color='orange', 
+                lw=1, 
+                ls='--', 
+                zorder=0)
+
+    #set ranges for values
+    ax1.set_ylim([yMin, yMax])
+    ax1.set_xlim(xmin=0.0, xmax=targetXTicks-1)
     
-    ax1.set_ylim([0, 80])
+    #set lables
     ax1.set_xlabel('Time')
     ax1.set_ylabel('Ping (in ms)')
     plt.title('Ping')
@@ -77,9 +93,10 @@ def animate(i):
     minPatch = mpatches.Patch(color='green', label='Min')
     avgPatch = mpatches.Patch(color='orange', label='Avg')
     maxPatch = mpatches.Patch(color='red', label='Max')
+    avgAvgPatch = mpatches.Patch(color='orange', label='Average Avg', linestyle='--', fill=False)
     
     plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.035),
-               ncol=3, fancybox=True, shadow=True, handles=[minPatch, avgPatch, maxPatch])
+               ncol=3, fancybox=True, shadow=True, handles=[minPatch, avgPatch, maxPatch, avgAvgPatch])
 
     for n, label in enumerate(ax1.xaxis.get_ticklabels()):
         if df.iloc[n]['Label?'] == False:
@@ -88,6 +105,8 @@ def animate(i):
 
 ani = animation.FuncAnimation(fig, animate, interval=1000)
 
-plt.gcf().subplots_adjust(bottom=0.15, left=0.15)
+plt.gcf().subplots_adjust(bottom=0.15, left=0.15, top=.9)
+plt.xticks(rotation=45)
 plt.show()
+
 
